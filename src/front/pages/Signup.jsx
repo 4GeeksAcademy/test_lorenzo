5671efe93b8e815286f785b97bc18b13bd7cb041
Santiago Ispizua <129971384+sispizua@users.noreply.github.com>
2026-03-05 16:43:59 +0000

    actualizacion de .nv token para el mapa

--- src/front/pages/Signup.jsx
+++ src/front/pages/Signup.jsx
@@ -127,7 +127,7 @@ export const Signup = () => {
                                     className="btn btn-outline-secondary"
                                     onClick={() => setShowConfirmPassword((s) => !s)}>
                                     <i className={`fa-solid ${showConfirmPassword ? "fa-eye-slash" : "fa-eye"}`} />
-                                </button>git
+                                </button>
                             </div>
 
                         </div>
